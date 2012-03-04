e93ab715c7860c894b8c722e0bb0fa160ca07da0
Yuri Leikind <yuri.leikind@gmail.com>
2012-03-04 18:09:31 +0100

    fixed a test

--- spec/requests/basics4_request_spec.rb
+++ spec/requests/basics4_request_spec.rb
@@ -6,11 +6,10 @@ describe "basisc4 WiceGrid", :type => :request, :js => true do
     visit '/basics4'
   end
 
-  # include_examples "basic task table specs"
-  # include_examples "show all and back"
-  # include_examples "names of columns"
-  # include_examples "sorting columns"
-  # include_examples "sorting columns in all records mode"
-  include_examples 'basic filtering'
+  include_examples "basic task table specs"
+  include_examples "show all and back"
+  include_examples "names of columns"
+  include_examples "sorting columns"
+  include_examples "sorting columns in all records mode"
 
 end
