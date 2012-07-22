06f5e70d6c226a223e516c257258be8a28d88ac1
Yuri Leikind <yuri.leikind@gmail.com>
2012-07-22 23:13:04 +0200

    tests fixed

--- spec/requests/resultset_processings2_request_spec.rb
+++ spec/requests/resultset_processings2_request_spec.rb
@@ -8,36 +8,47 @@ describe "with_resultset WiceGrid", :type => :request, :js => true do
 
   it "should show records displayed on all pages" do
 
-    page.should_not have_content('records on all pages')
+    within '.example' do
+      page.should_not have_content('records on all pages')
+    end
 
     find(:css, '#process').click
 
-    page.should have_content('50 records on all pages: 507, 519, 537, 540, 511, 515, 523, 524, 527, 531, 542, 551, 518, 520, 532, 535, 539, 512, 514, 516, 521, 522, 543, 544, 546, 550, 552, 510, 541, 553, 508, 513, 528, 529, 548, 556, 525, 534, 547, 555, 509, 517, 526, 536, 538, 545, 549, 530, 533, and 554')
+    within '.example' do
+      page.should have_content('50 records on all pages: 507, 519, 537, 540, 511, 515, 523, 524, 527, 531, 542, 551, 518, 520, 532, 535, 539, 512, 514, 516, 521, 522, 543, 544, 546, 550, 552, 510, 541, 553, 508, 513, 528, 529, 548, 556, 525, 534, 547, 555, 509, 517, 526, 536, 538, 545, 549, 530, 533, and 554')
+    end
 
   end
 
   it "should show records displayed on all pages with a text filter selection" do
 
-    page.should_not have_content('records on all pages')
+    within '.example' do
+      page.should_not have_content('records on all pages')
+    end
 
     fill_in('g_f_title', :with => 'ed')
     sleep 1
     find(:css, '#process').click
-    page.should have_content('2 records on all pages: 507 and 534')
 
+    within '.example' do
+      page.should have_content('2 records on all pages: 507 and 534')
+    end
   end
 
 
   it "should show records displayed on all pages with a custom filter selection" do
 
-    page.should_not have_content('records on all pages')
+    within '.example' do
+      page.should_not have_content('records on all pages')
+    end
 
     select 'Cancelled',  :from => 'g_f_status_id'
 
     find(:css, '#process').click
 
-    page.should have_content('8 records on all pages: 511, 515, 523, 524, 527, 531, 542, and 551')
+    within '.example' do
+      page.should have_content('8 records on all pages: 511, 515, 523, 524, 527, 531, 542, and 551')
+    end
 
   end
-
 end
